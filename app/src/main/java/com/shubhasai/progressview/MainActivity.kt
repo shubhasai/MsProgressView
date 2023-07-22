6714f57fbd3fc1702ffbcdd083450446fddfcaa8
Shubhasai <mshubhasai2002@gmail.com>
2023-07-22 06:33:14 +0530

    version-0.0.1 release

--- app/src/main/java/com/shubhasai/progressview/MainActivity.kt
+++ app/src/main/java/com/shubhasai/progressview/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import com.shubhasai.progress_view.CircularProgress
 import com.shubhasai.progressview.ui.theme.ProgressViewTheme
 
 class MainActivity : ComponentActivity() {
